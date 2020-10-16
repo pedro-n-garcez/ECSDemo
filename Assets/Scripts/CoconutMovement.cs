c2f7fde1dd45feb62b54daf42594a88c8a35ae29
pedro-n-garcez <60759070+pedro-n-garcez@users.noreply.github.com>
2020-10-16 04:22:10 -0300

    Initial commit
    -Unity 3D ECS demo added
    -Scene with user input, custom low-poly tree model

---
+++ Assets/Scripts/CoconutMovement.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Physics;
+using UnityEngine;
+
+//component
+[GenerateAuthoringComponent]
+public struct CoconutSpeedData : IComponentData
+{
+    public float speed;
+}
+
+//system
+[AlwaysSynchronizeSystem]
+public class CoconutMovementSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+        float2 input = new float2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        //change velocity for coconut speed data for all entities
+        Entities.ForEach((ref PhysicsVelocity velocity, in CoconutSpeedData csd) => 
+        {
+            float2 newvelocity = velocity.Linear.xz;
+            newvelocity += input * csd.speed * deltaTime;
+            velocity.Linear.xz = newvelocity;
+        }).Run();
+        return default;
+    }
+}
---
+++ Assets/Scripts/TreeSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
+
+//component
+[GenerateAuthoringComponent]
+public struct TreeSpeedData : IComponentData
+{
+    public float speed;
+}
+
+//system
+[AlwaysSynchronizeSystem]
+public class TreeSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+		float deltaTime = Time.DeltaTime;
+		//rotate along Z axis for Tree speed for all entities
+		Entities.ForEach((ref Rotation rotation, in TreeSpeedData rotationSpeed) =>
+		{
+			rotation.Value = math.mul(rotation.Value, quaternion.RotateZ(rotationSpeed.speed * deltaTime));
+		}).Run();
+		return default;
+    }
+}
